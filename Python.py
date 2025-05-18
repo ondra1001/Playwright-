da934b6879b3fa0931ba32973cad0aea80b26096
Ondřej Gryc <ondrejgrycw@gmail.com>
2025-05-18 16:10:22 +0200

    Napsání dvou testů

--- Python.py
+++ Python.py
@@ -1,6 +1,25 @@
 import pytest
 
-def test_title_page(page):
-    page.goto("https://www.google.com/")
-    title = page.locator("title")
-    assert title.inner_text() == "Google"
+URL = "https://engeto.cz/"
+
+def test_konzultace(page):
+    page.goto(URL)
+    button = page.locator("#top-menu > li:nth-child(8) > a")
+    button.click()
+    button = page.locator("body > main > div:nth-child(1) > div > div > a")
+    text = page.locator("#kontakt > div.form-company__head.flex.flex-column.gap-20.text-centered > h2")
+
+    assert text.inner_text() == "S\xa0čím můžeme pomoct?"
+
+
+def test_kurzy(page):
+    page.goto(URL)
+    button = page.locator("//*[@id='top-menu']/li[3]/a")
+    button.hover()
+    button = page.locator("//*[@id='top-menu']/li[3]/ul/li[2]/ul/li[1]/a/span")
+    button.click()
+    text = page.locator("main > div.full-page-width > div > div.flex-mobile-column > div > div.flex-column > h2")
+
+    assert text.inner_text() == "Tester s\xa0Pythonem"
+
+def 
