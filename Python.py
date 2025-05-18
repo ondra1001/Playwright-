0f74b6c36f717a70e42ba8ab7d853699719144ef
Ondřej Gryc <ondrejgrycw@gmail.com>
2025-05-18 18:48:04 +0200

    dodělán třetí test

--- Python.py
+++ Python.py
@@ -22,4 +22,18 @@ def test_kurzy(page):
 
     assert text.inner_text() == "Tester s\xa0Pythonem"
 
-def 
+def test_nevalidni_prihlaseni(page):
+    page.goto(URL)
+    button = page.locator('//*[@id="main-header"]/div/div/a')
+    button.click()
+    button = page.locator('body > main > div > div > a')
+    button.click()
+    email = page.locator('//*[@id="username"]')
+    email.fill("Tom")
+    heslo = page.locator('//*[@id="password"]')
+    heslo.fill("tom123")
+    pokracovat = page.locator('body > div > main > section > div > div > div > form > div.c5d45fb63 > button')
+    pokracovat.click()
+    oznameni = page.locator('//*[@id="error-element-password"]')
+
+    assert oznameni.inner_text() == "Nesprávný e-mail nebo heslo"
